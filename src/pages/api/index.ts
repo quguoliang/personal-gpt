b87a3b4ec42a130e78db839ef217f5aa6a007f0b
quguoliang@bytedance.com <quguoliang@bytedance.com>
2023-06-14 15:32:47 +0800

    feat: 新增语音对话功能& 支持语音录制和语音播放

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,21 +1,3 @@
-import { Blob } from 'buffer';
-
-/** string转换成blob */
-export function stringToBlob(str: string): any {
-  let arr = str.split(','),
-    fileType = arr[0]?.match(/:(.*?);/)[1],
-    bstr = atob(arr[1]),
-    l = bstr.length,
-    u8Arr = new Uint8Array(l);
-
-  while (l--) {
-    u8Arr[l] = bstr.charCodeAt(l);
-  }
-  return new Blob([u8Arr], {
-    type: fileType,
-  });
-}
-
 export function dataURLtoFile(dataurl, filename) {
   //将base64转换为文件
   var arr = dataurl.split(','),
